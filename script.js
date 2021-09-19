c28b99fde699d8d1e98d510cd2d5b22d6170b30d
jamtibaes <jamtibaes@yahoo.com.br>
2021-09-19 17:56:35 -0300

    final project fixed

--- script.js
+++ script.js
@@ -17,12 +17,25 @@ function compute()
         document.getElementById("result").innerHTML = "<br>If you deposit <mark> " + principal + "</mark> ,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
     };
 
-   
-    
 }
 
 function updateRate(){
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
-        
+
+function clean(){
+    // This Function clean all the previous informations on input tags
+    document.getElementById("principal").value = "";
+    document.getElementById("principal").focus();
+    document.getElementById("rate").value = "12.25%";
+    document.getElementById("years").value = "1";
+    document.getElementById("rate_val").innerText = "12.25%";
+    document.getElementById("result").innerHTML = " ";
+}
+
+window.onload = function() {
+    //Dynamic inset the value e auto-focus principal 
+    document.getElementById("principal").focus();
+    document.getElementById("rate_val").innerText = "12.25%";
+}
